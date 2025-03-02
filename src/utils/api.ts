d771f29d43beaa5884ddfdbcb968ce18b7a61743
Alam Raga Deva <argadeva@gmail.com>
2025-03-03 03:49:01 +0700

    Remove Github Key Secure Deployment

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,18 +1,9 @@
 import { FindUsers, Repository } from '../types/github'
 
-const headers = {
-  Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_KEY}`,
-}
-
 export const fetchRepositories = async (
   username: string
 ): Promise<Repository[]> => {
-  const response = await fetch(
-    `https://api.github.com/users/${username}/repos`,
-    {
-      headers,
-    }
-  )
+  const response = await fetch(`https://api.github.com/users/${username}/repos`)
   if (!response.ok) {
     throw new Error('Failed to fetch repositories')
   }
@@ -24,10 +15,7 @@ export const fetchUsers = async (
   pageParam: number = 1
 ): Promise<FindUsers> => {
   const response = await fetch(
-    `https://api.github.com/search/users?q=${username}&page=${pageParam}&per_page=10`,
-    {
-      headers,
-    }
+    `https://api.github.com/search/users?q=${username}&page=${pageParam}&per_page=10`
   )
   if (!response.ok) {
     throw new Error('Failed to fetch users')
